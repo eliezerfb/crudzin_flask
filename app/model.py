fe4dcf593bb03234c24e135a0adfb7a588bfac02
Eliézer Bourchardt <eliezerfb@gmail.com>
2019-04-07 21:11:59 -0300

    add flask project and migrations

---
+++ app/model.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+def configure(app):
+    db.init_app(app)
+    app.db = db
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    livro = db.Column(db.String(255))
+    autor = db.Column(db.String(255))
---
+++ readme.md
@@ -0,0 +1,9 @@
+https://www.youtube.com/watch?v=WzaKIRJBGXo
+
+## Como rodar esse projeto:
+
+```sh
+export FLASK_APP=app
+export FLASK_ENV=Development
+export FLASK_DEBUG=True
+```
